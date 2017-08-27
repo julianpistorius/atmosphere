ff72473a058ad077373b8f3174a0b928b00d2eb4
Julian Pistorius <julian@julianpistorius.com>
2017-08-27 16:20:16 -0700

    Moved `AbstractSelfServiceModelAdmin` to `selfservice` module.

--- jetstream/admin.py
+++ jetstream/admin.py
@@ -1,42 +1,17 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.admin.options import csrf_protect_m
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
 
 from jetstream import models
-from jetstream.selfservice import TASAPIQuery, run_tas_api_query
+from jetstream.selfservice import TASAPIQuery, run_tas_api_query, AbstractSelfServiceModelAdmin
 
 
 @admin.register(models.TASAllocationReport)
 class TASReportAdmin(admin.ModelAdmin):
-    search_fields = ["project_name", "username",]
+    search_fields = ["project_name", "username", ]
     list_display = ["id", "username", "project_name", "compute_used", "start_date", "end_date", "success"]
     list_filter = ["success", "project_name"]
-
-
-class AbstractSelfServiceModelAdmin(admin.ModelAdmin):
-    def get_urls(self):
-        info = self.model._meta.app_label, self.model._meta.model_name
-        changelist_url_name = '%s_%s_changelist' % info
-        urls = [
-            url(r'^$', self.admin_site.admin_view(run_tas_api_query), name=changelist_url_name),
-        ]
-        return urls
-
-    def changelist_view(self, request, extra_context=None):
-        """
-        The 'change list' admin view for a 'fake' model.
-
-        Subclass this class and implement this method yourself.
-        """
-        raise NotImplementedError
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 
 @admin.register(TASAPIQuery)
--- jetstream/selfservice.py
+++ jetstream/selfservice.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db import models
 from django.forms import ModelForm, Textarea
@@ -88,3 +90,27 @@ def _execute_tas_api_query(query_type, query=None):
             info = e
 
     return info, header, rows
+
+
+class AbstractSelfServiceModelAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        info = self.model._meta.app_label, self.model._meta.model_name
+        changelist_url_name = '%s_%s_changelist' % info
+        urls = [
+            url(r'^$', self.admin_site.admin_view(run_tas_api_query), name=changelist_url_name),
+        ]
+        return urls
+
+    def changelist_view(self, request, extra_context=None):
+        """
+        The 'change list' admin view for a 'fake' model.
+
+        Subclass this class and implement this method yourself.
+        """
+        raise NotImplementedError
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
